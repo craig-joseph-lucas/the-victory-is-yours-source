45916dbb5413447900dd324bb763132e2c2162b4
clucaseh <clucas@everydayhealthinc.com>
2020-04-02 10:04:09 -0700

    feat(sitemap): adding sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   pathPrefix: siteConfig.pathPrefix,
   siteMetadata: {
     url: siteConfig.url,
+    siteUrl: siteConfig.url,
     title: siteConfig.title,
     subtitle: siteConfig.subtitle,
     copyright: siteConfig.copyright,
@@ -16,6 +17,9 @@ module.exports = {
     facebookAppId: siteConfig.facebookAppId
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
