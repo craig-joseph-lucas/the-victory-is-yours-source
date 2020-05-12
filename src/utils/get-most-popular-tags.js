da2b6a76d6ceb581b6ac9aca4585d4b6754c009c
clucaseh <clucas@everydayhealthinc.com>
2020-05-12 11:18:30 -0700

    feat(tags): util for getting most popular tags

---
+++ src/utils/get-most-popular-tags.js
@@ -0,0 +1,16 @@
+import { useTagsList } from '../hooks';
+
+function getSlug (topic) {
+    const cleanTopic = topic.toLowerCase().replace(/\s/g, '-');
+    return `/tags/${cleanTopic}`;
+}
+
+export default function getMostPopularTags(count) {
+    const tagList = useTagsList();
+    let tags = tagList.sort((a, b) => (a.totalCount < b.totalCount) ? 1 : -1);
+    tags = tags.slice(0, count);
+    tags.forEach(element => {
+        element.url = getSlug(element.fieldValue);
+    });
+    return tags;
+}
